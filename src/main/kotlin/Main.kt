d6716cd53ad09649ef0c10824a01b77f1b7b96e0
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-03-10 17:41:50 +0100

    Finish ex.3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 fun main(args: Array<String>) {
-    val student: Student = Student();
-    println(student.name)
+    val student1 = Student("Mike")
+    val student2 = Student("Alicja", "Uniwersytet Warszawski")
+    println(student1.name + " " + student1.university)
+    println(student2.name + " " + student2.university)
 }
 
-class Student {
-    val name = "Mike"
+class Student(name: String, val university: String = "Politechnika Krakowska") {
+    val name: String = name
         get() {
            return field.replaceFirstChar { c -> c.uppercaseChar() }
         }
